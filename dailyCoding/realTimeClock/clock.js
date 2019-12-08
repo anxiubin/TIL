3d914212170b994b7106850e162368f7d49b6a16
anxiubin <tnqls0120@naver.com>
2019-12-08 23:55:20 +0900

    12/08 js real time clock

---
+++ dailyCoding/realTimeClock/clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector('.js-clock'),
+    clockTitle = clockContainer.querySelector('h1');
+
+//시간 가져오는 함수
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+        minutes < 10? `0${minutes}` : minutes
+    } : ${seconds < 10 ? `0${seconds}` : seconds}`
+}
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000); //1초마다 getTime 함수 실행하기
+}
+
+init(); 
